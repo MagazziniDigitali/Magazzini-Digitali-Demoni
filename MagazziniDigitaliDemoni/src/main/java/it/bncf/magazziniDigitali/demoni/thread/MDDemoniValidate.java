e3f7b3a0e536a2300e9fc0fc713c1a77a093add4
Massimiliano Randazzo <massimiliano.randazzo@gmail.com>
2014-06-01 22:36:29 +0200

    Implementato l'utilizzo delle librerie Buisness e il tracciato Premis per la registrazione degli eventi

--- MagazziniDigitaliDemoni/src/main/java/it/bncf/magazziniDigitali/demoni/thread/MDDemoniValidate.java
+++ MagazziniDigitaliDemoni/src/main/java/it/bncf/magazziniDigitali/demoni/thread/MDDemoniValidate.java
@@ -3,17 +3,7 @@
  */
 package it.bncf.magazziniDigitali.demoni.thread;
 
-import it.bncf.magazzimiDigitali.database.entity.MDFilesTmp;
-import it.bncf.magazzimiDigitali.databaseSchema.sqlite.MDFilesTmpSqlite;
-import it.bncf.magazziniDigitali.demoni.thread.validate.ValidateFile;
-
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.sql.SQLException;
-import java.util.List;
-
-import mx.randalf.configuration.Configuration;
-import mx.randalf.configuration.exception.ConfigurationException;
+import it.bncf.magazziniDigitali.businessLogic.oggettoDigitale.OggettoDigitaleBusiness;
 
 import org.apache.log4j.Logger;
 
@@ -50,90 +40,9 @@ public class MDDemoniValidate extends Thread {
 	 * 
 	 */
 	private void execute() {
-		MDFilesTmpSqlite mdFileTmp = null;
-		List<MDFilesTmp> rs = null;
-		String fileObj = null;
-		File fObj = null;
-		ValidateFile validate = null;
+		OggettoDigitaleBusiness odBusiness = null;
 
-		try {
-			mdFileTmp = new MDFilesTmpSqlite();
-
-			// Eseguo la ricerca di tutti i files che hanno finito il
-			// trasferimento oppure che risultano in fase di validazione
-			rs = mdFileTmp.findByStatus(new String[] {
-					MDFilesTmpSqlite.FINETRASF, MDFilesTmpSqlite.INITVALID });
-			if (rs != null && rs.size() > 0) {
-				// Risulta almeno 1 record da elaborare
-				validate = new ValidateFile();
-				for (int x = 0; x < rs.size(); x++) {
-					try {
-						// calcolo il file da validare
-						fileObj = Configuration.getValue("istituto."
-								+ rs.get(x).getIdIstituto() + ".pathTmp");
-						fileObj += File.separator;
-						fileObj += rs.get(x).getNomeFile();
-						fObj = new File(fileObj);
-						log.debug("fileObj: " + fObj.getAbsolutePath());
-						if (fObj.exists()) {
-							// il file Esiste
-							if (rs.get(x).getStato()
-									.equals(MDFilesTmpSqlite.FINETRASF)) {
-								log.debug("Inizio la validazione del file ["
-										+ fObj.getAbsolutePath() + "]");
-								mdFileTmp.updateStartValidate(rs.get(x).getId());
-							} else {
-								log.debug("Continuo la validazione del file ["
-										+ fObj.getAbsolutePath() + "]");
-							}
-							validate.check(fObj);
-							if (validate.isErrors()) {
-								mdFileTmp.updateStopValidate(rs.get(x).getId(),
-										null, false, validate.getErrors());
-							} else {
-								mdFileTmp.updateStopValidate(rs.get(x).getId(),
-										validate.getXmlType().value(), true,
-										null);
-							}
-						} else {
-							mdFileTmp.updateStopValidate(rs.get(x).getId(),
-									null, false, new String[] { "Il file ["
-											+ fObj.getAbsolutePath()
-											+ "] non è presente sul Server" });
-						}
-					} catch (ConfigurationException e) {
-						mdFileTmp.updateStopValidate(
-								rs.get(x).getId(),
-								null,
-								false,
-								new String[] { e.getMessage() });
-						log.error(e.getMessage(), e);
-					} catch (SQLException e) {
-						mdFileTmp.updateStopValidate(
-								rs.get(x).getId(),
-								null,
-								false,
-								new String[] { e.getMessage() });
-						log.error(e.getMessage(), e);
-					}
-				}
-			}
-		} catch (FileNotFoundException e) {
-			log.error(e.getMessage(), e);
-		} catch (ClassNotFoundException e) {
-			log.error(e.getMessage(), e);
-		} catch (SQLException e) {
-			log.error(e.getMessage(), e);
-		} catch (ConfigurationException e) {
-			log.error(e.getMessage(), e);
-		} finally {
-			try {
-				if (mdFileTmp != null) {
-					mdFileTmp.disconnect();
-				}
-			} catch (SQLException e) {
-				log.error(e.getMessage(), e);
-			}
-		}
+		odBusiness = new OggettoDigitaleBusiness();
+		odBusiness.validate("Validate");
 	}
 }
