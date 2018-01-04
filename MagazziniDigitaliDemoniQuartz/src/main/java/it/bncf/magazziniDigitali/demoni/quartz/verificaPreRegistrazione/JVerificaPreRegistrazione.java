27d2be1f72519b5b120dc94c6e1fdf6618e3b810
Massimiliano Randazzo <massimiliano.randazzo@gmail.com>
2018-01-04 17:35:12 +0100

    Aggiornamento

--- MagazziniDigitaliDemoniQuartz/src/main/java/it/bncf/magazziniDigitali/demoni/quartz/verificaPreRegistrazione/JVerificaPreRegistrazione.java
+++ MagazziniDigitaliDemoniQuartz/src/main/java/it/bncf/magazziniDigitali/demoni/quartz/verificaPreRegistrazione/JVerificaPreRegistrazione.java
@@ -3,13 +3,10 @@
  */
 package it.bncf.magazziniDigitali.demoni.quartz.verificaPreRegistrazione;
 
-import java.io.IOException;
-
 import org.apache.log4j.Logger;
 import org.hibernate.HibernateException;
 import org.quartz.JobExecutionContext;
 import org.quartz.JobExecutionException;
-import org.quartz.SchedulerException;
 
 import it.bncf.magazziniDigitali.configuration.exception.MDConfigurationException;
 import it.bncf.magazziniDigitali.database.dao.MDPreRegistrazioneDAO;
@@ -74,9 +71,9 @@ public class JVerificaPreRegistrazione extends JobExecute {
 //		} catch (SchedulerException e) {
 //			log.error(e.getMessage(), e);
 //			throw new JobExecutionException(e.getMessage(), e);
-		} catch (IOException e) {
-			log.error(e.getMessage(), e);
-			throw new JobExecutionException(e.getMessage(), e);
+//		} catch (IOException e) {
+//			log.error(e.getMessage(), e);
+//			throw new JobExecutionException(e.getMessage(), e);
 		} catch (HibernateUtilException e) {
 			log.error(e.getMessage(), e);
 			throw new JobExecutionException(e.getMessage(), e);
